ff67cea651c606f26a205c79cf6989d0004f0a83
Yaroslav50 <z7664008@gmail.com>
2024-10-14 17:27:39 +0300

    introduction to JS. Lesson 1

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("test");
+let a = 5;
+var b = true;
+const c = "WORD";
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// Приведення до числа
+
+const value = '3';
+console.log(typeof value);
+const numberedValue = Number(value);
+console.log(numberedValue);
+console.log(typeof numberedValue);
+
+// Gеревіркм на число
+
+let d = 31;
+console.log(a);
+const number = Number('51');
+console.log(Number.isNaN(number));
+
+const string = Number('qweqwe');
+console.log(Number.isNaN(string));
