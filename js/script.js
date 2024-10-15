eb85ae1a26d2d97d9d26b3a3b547fa332894c763
Yaroslav50 <z7664008@gmail.com>
2024-10-15 17:46:01 +0300

    just introduction to js learning

--- js/script.js
+++ js/script.js
@@ -1,25 +1,74 @@
-console.log("test");
+// console.log("Перший рядок");
+
+// Типи
+
+// number BigInt string boolean null undefined naN Object Symbol
+
+/*  
 let a = 5;
 var b = true;
 const c = "WORD";
 console.log(typeof a);
 console.log(typeof b);
-console.log(typeof c);
+console.log(typeof c); 
+*/
 
 // Приведення до числа
 
+/*
 const value = '3';
 console.log(typeof value);
-const numberedValue = Number(value);
-console.log(numberedValue);
-console.log(typeof numberedValue);
+console.log(Number(value));
+console.log(typeof Number(value));
+*/
 
-// Gеревіркм на число
+// Перевірка на число
 
-let d = 31;
+/*
+let a = 31;
 console.log(a);
 const number = Number('51');
 console.log(Number.isNaN(number));
 
 const string = Number('qweqwe');
 console.log(Number.isNaN(string));
+*/
+
+// Number.parseInt() і Number.parseFloat()
+
+/*
+console.log(Number.parseInt('j4ggggg'));
+console.log(Number.parseInt('5ddd'));
+console.log(Number.parseInt('12qg994'));
+console.log(Number.parseInt('11.6qwfe79'));
+console.log(Number.parseInt('zzxxc'));
+*/
+
+/*
+console.log(Number.parseInt('j4ggggg'));
+console.log(Number.parseFloat('5.7689405ddd'));
+console.log(Number.parseFloat('12.7qg994'));
+console.log(Number.parseFloat('11.60000qwfe79'));
+console.log(Number.parseFloat('0.878zzxxc'));
+*/
+
+// додавання з крапкою
+
+/*
+console.log(0.1 + 0.2 === 0.3);
+console.log(0.1 + 0.2);
+console.log((0.1*10 + 0.2*10)/10);
+*/
+
+// Math object
+/*
+console.log(Math.floor(3.98)); //3
+console.log(Math.ceil(0.01)); //1
+console.log(Math.round(1.2)); //1
+console.log(Math.round(1.5)); //2
+console.log(Math.max(1,5,99,54,76,23)); //99
+console.log(Math.min(99,76,55.4,0.1,1,5)); //0.1
+console.log(Math.pow(2, 4)); //16
+console.log(Math.random() * (10 - 1) + 1); // дробове
+console.log(Math.floor(Math.random() * (10 - 1) + 1)); // ціле
+*/
